0ce74e1c68476a8777d449748b98e3ceb426c0ec
Pavel Koryakin <unclekoin@gmail.com>
2022-02-03 15:23:44 +0300

    feat: add update data method

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 const notes = document.querySelectorAll('.list-group-item-text');
+const hidden = 'visually-hidden';
 
 document.addEventListener('click', (e) => {
   if (e.target.dataset.type === 'remove') {
@@ -10,40 +11,47 @@ document.addEventListener('click', (e) => {
   }
 
   if (e.target.dataset.type === 'edit') {
-    // const id = e.target.dataset.id;
+    const id = e.target.dataset.id;
     const container = e.target.closest('.list-group-item');
     const noteContent = container.querySelector('.list-group-item-text');
     const cancelBtn = createElement('button', ['btn', 'btn-outline-danger'], 'cancel', 'Cancel');
-    const saveBtn = createElement('button', ['btn', 'btn-outline-success',  'me-2'], 'save', 'Save');
-    const input = createElement('input', ['form-control', 'me-2'], '');
+    const saveBtn = createElement('button', ['btn', 'btn-outline-success', 'me-2'], 'save', 'Save');
+    const input = createElement('input', ['form-control', 'me-2'], '', '', id);
     input.value = noteContent.textContent;
     const removeBtn = e.target.parentElement.querySelector('span');
-    removeBtn.classList.add('visually-hidden');
+    removeBtn.classList.add(hidden);
     e.target.parentElement.append(saveBtn);
     e.target.parentElement.append(cancelBtn);
     container.prepend(input);
-    noteContent.classList.add('visually-hidden');
-    e.target.classList.add('visually-hidden');
+    input.focus();
+    noteContent.classList.add(hidden);
+    e.target.classList.add(hidden);
   }
 
   if (e.target.dataset.type === 'cancel') {
-    const container = e.target.closest('.list-group-item');
-    const saveBtn = container.querySelector('button[data-type=save]')
-    const updateBtn = container.querySelector('button[data-type=edit]')
-    const removeBtn = e.target.parentElement.querySelector('span');
-    const noteContent = container.querySelector('.list-group-item-text');
-    const input = container.querySelector('input');
-    removeBtn.classList.remove('visually-hidden');
+    const [input, saveBtn, updateBtn, removeBtn, noteContent] = getElements(e.target, 'save');
+    removeBtn.classList.remove(hidden);
     saveBtn.remove();
-    updateBtn.classList.remove('visually-hidden')
-    noteContent.classList.remove('visually-hidden');
+    updateBtn.classList.remove(hidden);
+    noteContent.classList.remove(hidden);
     input.remove();
     e.target.remove();
   }
 
   if (e.target.dataset.type === 'save') {
-    const id = 123
-    console.log('save', id);
+    const [input, cancelBtn, updateBtn, removeBtn, noteContent] = getElements(e.target, 'cancel');
+    const id = input.id;
+    const title = input.value;
+
+    update(id, title).then(() => {
+      noteContent.classList.remove(hidden);
+      noteContent.textContent = title;
+      input.remove();
+      cancelBtn.remove();
+      updateBtn.classList.remove(hidden);
+      removeBtn.classList.remove(hidden);
+      e.target.remove();
+    })
   }
 });
 
@@ -65,4 +73,14 @@ function createElement(tag = 'div', classes = [], type = '', content = '', id = 
   if (content) element.textContent = content;
 
   return element;
+}
+
+function getElements(root, type) {
+  const container = root.closest('.list-group-item');
+  const input = container.querySelector('input');
+  const btn = container.querySelector(`button[data-type=${ type }]`);
+  const updateBtn = container.querySelector('button[data-type=edit]');
+  const removeBtn = container.querySelector('span[data-type=remove]');
+  const noteContent = container.querySelector('.list-group-item-text');
+  return [input, btn, updateBtn, removeBtn, noteContent];
 }
